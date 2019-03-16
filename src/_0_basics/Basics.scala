ee8661c99a009f59b1597d2d4c59d10957686f54
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-03-16 22:33:18 +0100

    basics: comments, variables and print

--- src/_0_basics/Basics.scala
+++ src/_0_basics/Basics.scala
@@ -1,16 +1,23 @@
 object Basics {
   def main(args: Array[String]) {
-
-    // variables
-    // val variables are able to changed
-    var scalaHelloWorld = "Hello From Scala World"
     
+    // Hello World! 
+    // print on the screen
+    println("Hello From Scala World")
+    
+    // Comments :
+    // This is one line comment.
+    /**
+    * This is a multiple lines comment.
+    */
+
+    // Variables :
+    // var variables are able to changed
+    var myAge = 27
     // val variables are not able to changed
-    val myName = "MedAimane"
+    val myName = "Med Aimane"
     val myCity = "Tetouan"
 
-    println(scalaHelloWorld);
-    println(myName);
-    println(myCity)
+    println("My name's " + myName + ", I'm " + myAge + " and I live on " + myCity)
   }
 }
