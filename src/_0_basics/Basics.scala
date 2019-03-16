6d89d01d000724a0746c3869d074ea38654b7169
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-03-16 22:24:30 +0100

    var & val

--- src/_0_basics/Basics.scala
+++ src/_0_basics/Basics.scala
@@ -1,5 +1,16 @@
 object Basics {
   def main(args: Array[String]) {
-    println("Hello From Scala World");
+
+    // variables
+    // val variables are able to changed
+    var scalaHelloWorld = "Hello From Scala World"
+    
+    // val variables are not able to changed
+    val myName = "MedAimane"
+    val myCity = "Tetouan"
+
+    println(scalaHelloWorld);
+    println(myName);
+    println(myCity)
   }
 }
